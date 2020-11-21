bd84080028fa98587fa228805be2b9307c39c98f
Vitaly Ogoltsov <vitaly.ogoltsov@me.com>
2020-11-21 20:21:01 +0300

    refactor(helix-swarm): Removed excessive logging when adding trigger token.

--- helix-swarm/docker-startup.d/90-swarm-trigger-token.sh
+++ helix-swarm/docker-startup.d/90-swarm-trigger-token.sh
@@ -9,7 +9,7 @@ fi
 
 # create directory for tokens
 export SWARM_TOKENS_DIRECTORY="${SWARM_DATA_DIR}/queue/tokens"
-mkdir -pv "${SWARM_TOKENS_DIRECTORY}"
+mkdir -p "${SWARM_TOKENS_DIRECTORY}"
 
 # for a trigger token to be valid, there must exist an empty file with trigger token name in tokens directory
 touch "${SWARM_TOKENS_DIRECTORY}/${SWARM_TRIGGER_TOKEN}"
