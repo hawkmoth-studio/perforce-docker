cd31cf46d86854086f4d72556762a270dd2c7772
Vitaly Ogoltsov <vitaly.ogoltsov@me.com>
2021-01-26 02:51:28 +0300

    fix(helix-p4d): Added verbose logging to docker-startup.d/01-permissions.sh.

--- helix-p4d/docker-startup.d/01-permissions.sh
+++ helix-p4d/docker-startup.d/01-permissions.sh
@@ -3,10 +3,12 @@
 set -e
 
 # all files under P4ROOT must be owned by perforce system user
+echo "Checking permissions in ${P4ROOT}"
 mkdir -p "${P4ROOT}"
 chown -R perforce:perforce "${P4ROOT}"
 
 # all files under ssl directory should have strict permissions set
+echo "Checking permissions in ${P4SSLDIR}"
 mkdir -p "${P4SSLDIR}"
 chown -R perforce:perforce "${P4SSLDIR}"
 chmod 0700 "${P4SSLDIR}"
