99371735684e26dc6aa729316d8212e54238d8d4
Vitaly Ogoltsov <vitaly.ogoltsov@me.com>
2025-06-22 15:44:42 +0200

    refactor: Added info message when database upgrade is run.

--- helix-p4d/docker-startup.d/45-database-upgrade.sh
+++ helix-p4d/docker-startup.d/45-database-upgrade.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-# check that the p4d service is already configured
-if ! gosu perforce p4dctl list 2>/dev/null | grep -q "${P4NAME}"; then
-    exit 0
-fi
-
 # only perform database upgrade if explicitly allowed
 if [[ "${P4D_DATABASE_UPGRADE}" != "true" ]]; then
     exit 0
 fi
 
+# check that the p4d service is already configured
+if ! gosu perforce p4dctl list 2>/dev/null | grep -q "${P4NAME}"; then
+    exit 0
+fi
+
 # enable database upgrade on next run
+echo "Running database upgrade procedure..."
 gosu perforce p4dctl exec -t p4d "${P4NAME}" -- -xu
